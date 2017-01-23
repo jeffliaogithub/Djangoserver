a0970be854d81195bd491cc12557a7ab7e034457
jeffliaogithub <jeffliao@yahoo.com>
2017-01-22 17:33:06 -0800

    remove unnecessary file

--- DjangoWebProject/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!J:\Users\jeff\Documents\Visual Studio 2015\Projects\DjangoWebProject\DjangoWebProject\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
