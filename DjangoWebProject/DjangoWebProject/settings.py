36318982651c435c88510607b64a6ac61c3e13b7
jeffliaogithub <jeffliao@yahoo.com>
2017-01-22 18:06:50 -0800

    move plotutil.cpp to app directory

--- DjangoWebProject/DjangoWebProject/settings.py
+++ DjangoWebProject/DjangoWebProject/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 import posixpath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print ("base dir path", BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR,"app"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
